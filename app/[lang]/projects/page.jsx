03ee79401009f996bf747e5280555aed96a2f6a5
Milad Mohammadi <Mohammadi.Dev@gmail.com>
2024-11-02 08:41:00 +0330

    Fix

--- app/[lang]/projects/page.jsx
+++ app/[lang]/projects/page.jsx
@@ -11,7 +11,6 @@ import TextTitleMedium from "@/app/components/text/TextTitleMedium";
 import TextTitleSmall from "@/app/components/text/TextTitleSmall";
 import { ProjectList } from "@/data/en/projectList";
 import { ProjectListFa } from "@/data/fa/projectList";
-import { constants } from "../../../data/Constants";
 import {
   Chip,
   Image,
@@ -26,12 +25,6 @@ import {
 } from "@nextui-org/react";
 import Link from "next/link";
 import { useState } from "react";
-
-export async function generateStaticParams() {
-  return constants.locales.map((locale) => ({
-    locale,
-  }));
-}
 
 export default function Home({ params: { lang } }) {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
